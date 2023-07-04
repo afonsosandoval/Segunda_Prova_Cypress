45d0c0b6968169eeb96cc0e5e3a99fd1f1678462
afonsosandoval@gmail.com <afonsosandoval@gmail.com>
2023-07-04 16:18:05 -0300

    EXCLUSAO E INCLUSAO DO CARD

--- cypress/integration/Trello_API.spec.cy.js
+++ cypress/integration/Trello_API.spec.cy.js
@@ -1,11 +1,12 @@
 const token = 'ATTA4938d59d96ce176795927ca8195a211f27a1d2dd113edfcb50b2af142ffbaf74B56A94A4';
 const key = 'eaa78d6bb0dfd6297adcc72ecf80cd5f';
 let idBoard;
-let idcard;
+let idListBord;
 let idList;
-let nome = '';
+let idCard;
 let arrayList;
 const board = 'Board_Cypress'
+const card = 'Card_Board_Cypress'
 describe('Operações Trello conta Afonso Sandoval', ()=> {
 
     it('buscar dados do meu board', ()=> {
@@ -47,12 +48,41 @@ describe('Operações Trello conta Afonso Sandoval', ()=> {
            arrayList.forEach((item) => {
             const { id, name } = item; // Destructure the object properties
             if(item.name == 'To Do'){
-                idcard = item.id
+                idListBord = item.id
             }
         });
         }
         )
     })
+//https://api.trello.com/1/cards?idList=64a451f3d9768bf04cee9f50&key=eaa78d6bb0dfd6297adcc72ecf80cd5f&token=ATTA4938d59d96ce176795927ca8195a211f27a1d2dd113edfcb50b2af142ffbaf74B56A94A4&name=Teste_Cypress_Bord
+    it('incluir card no board gerado', ()=> {
+        cy.request({
+            method: 'POST',
+            url: 'https://api.trello.com/1/cards?idList='+ idListBord + '&key=' + key + '&token=' + token + '&name=' + card
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            idCard = res.body.id;
+
+         }
+ 
+         )
+    })
+
+    //https://api.trello.com/1/cards/{id}?key=APIKey&token=APIToken
+    it('Excluir card gerado', ()=> {
+
+        cy.request({
+            method: 'DELETE',
+            url: 'https://api.trello.com/1/cards/' + idCard + '?key=' + key + '&token=' + token
+        }).then((res) => {
+           expect(res.status).to.be.equal(200)
+         
+        }
+
+        )
+        
+    })
+
     it('Excluir board gerado', ()=> {
 
         cy.request({
@@ -60,8 +90,7 @@ describe('Operações Trello conta Afonso Sandoval', ()=> {
             url: 'https://api.trello.com/1/boards/' + idBoard + '?key=' + key + '&token=' + token
         }).then((res) => {
            expect(res.status).to.be.equal(200)
-           arrayList = res.body
-           cy.log('lista',arrayList)
+        
         }
 
         )
