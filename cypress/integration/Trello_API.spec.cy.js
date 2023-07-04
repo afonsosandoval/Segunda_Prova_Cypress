2579c51d0fe20c216c79e01b04764e6f1748c482
afonsosandoval@gmail.com <afonsosandoval@gmail.com>
2023-07-04 16:00:30 -0300

    pegar id card

--- cypress/integration/Trello_API.spec.cy.js
+++ cypress/integration/Trello_API.spec.cy.js
@@ -1,6 +1,10 @@
 const token = 'ATTA4938d59d96ce176795927ca8195a211f27a1d2dd113edfcb50b2af142ffbaf74B56A94A4';
 const key = 'eaa78d6bb0dfd6297adcc72ecf80cd5f';
-let id;
+let idBoard;
+let idcard;
+let idList;
+let nome = '';
+let arrayList;
 const board = 'Board_Cypress'
 describe('Operações Trello conta Afonso Sandoval', ()=> {
 
@@ -22,25 +26,42 @@ describe('Operações Trello conta Afonso Sandoval', ()=> {
 
         cy.request({
             method: 'POST',
-            url: 'https://api.trello.com/1/boards/?name=' + board + '&key=' + key + '&token=' + token,
+            url: 'https://api.trello.com/1/boards/?name=' + board + '&key=' + key + '&token=' + token
         }).then((res) => {
            expect(res.status).to.be.equal(200)
            expect(res.body).is.not.empty
            expect(res.body).to.have.property('id')
            expect(res.body.name).to.be.equal(board)
-           id = res.body.id;
+           idBoard = res.body.id;
         }
 
         )
     })
-
+    it('buscar idlist do board cadastrado', ()=> {
+        cy.request({
+            method: 'GET',
+            url: 'https://api.trello.com/1/boards/'+ idBoard + '/lists?fields=name&key=' + key + '&token=' + token
+        }).then((res) => {
+           expect(res.status).to.be.equal(200)
+           arrayList = res.body
+           arrayList.forEach((item) => {
+            const { id, name } = item; // Destructure the object properties
+            if(item.name == 'To Do'){
+                idcard = item.id
+            }
+        });
+        }
+        )
+    })
     it('Excluir board gerado', ()=> {
 
         cy.request({
             method: 'DELETE',
-            url: 'https://api.trello.com/1/boards/' + id + '?key=' + key + '&token=' + token
+            url: 'https://api.trello.com/1/boards/' + idBoard + '?key=' + key + '&token=' + token
         }).then((res) => {
            expect(res.status).to.be.equal(200)
+           arrayList = res.body
+           cy.log('lista',arrayList)
         }
 
         )
